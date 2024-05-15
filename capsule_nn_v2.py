3842f8af3b8b5dc9e628e7a15b743e710f85e7d2
Raki Delmoro <rakidelmoro@gmail.com>
2024-05-15 04:49:11 +0000

    Update.

--- capsule_nn_v2.py
+++ capsule_nn_v2.py
@@ -1,25 +1,26 @@
+import math
 import torch
 import torch.nn as nn
 from torch import Tensor
 
 class CapsuleNeuralNetworkV2(nn.Module):
-    def __init__(self, capsule_wide, capsule_tall):
+    def __init__(self, feature_sizes, capsule_wide, capsule_tall):
         super().__init__()
-        # self.layers = []
-        # for i in range(len(feature_sizes)-1):
-        #     layers = nn.Sequential(nn.Linear(feature_sizes[i], feature_sizes[i+1], device="cuda"))
-        #     self.layers.append(layers)
+        self.layers = []
+        for i in range(len(feature_sizes)-1):
+            assert feature_sizes[i] % capsule_tall == 0, f"{capsule_tall} should divisible by {feature_sizes[i]}"
+            input_feature_view = feature_sizes[i] // capsule_tall
+            output_feature_view = feature_sizes[i+1] // capsule_tall
+            capsule_tall_layer = []
+            for _ in range(capsule_tall):
+                input_view_layer = nn.Linear(input_feature_view, output_feature_view, device="cuda")
+                capsule_tall_layer.append(input_view_layer)
+            layer = nn.Sequential(nn.Sequential(*capsule_tall_layer))
+            self.layers.append(layer)
 
-        self.first_copy = nn.Linear(784//capsule_tall, 784//capsule_tall, device="cuda")
-        self.second_copy = nn.Linear(784//capsule_tall, 784//capsule_tall, device="cuda")
-        self.third_copy = nn.Linear(784//capsule_tall, 784//capsule_tall, device="cuda")
-
-        self.num_capsule = 4
         self.capsule_tall = capsule_tall
         self.capsule_wide = capsule_wide
-
-        self.output_decoder = nn.Sequential(nn.Linear(784, 784, device="cuda"), nn.ReLU(), nn.Linear(784, 784, device="cuda"))
-        self.output_probability = nn.Sequential(nn.Linear(784, 10, device="cuda"), nn.Softmax(-1))
+        self.output_decoder = nn.Linear(feature_sizes[0], 10, device="cuda")
 
     def apply_capsule_tall_dim(self, x: Tensor):
         assert x.shape[-1] % self.capsule_tall == 0, f"Input feature {x.shape[-1]} should divisible by {self.capsule_tall}"
@@ -27,27 +28,18 @@ class CapsuleNeuralNetworkV2(nn.Module):
 
         # batch | capsule tall | feature view
         new_input_shape = x.shape[:-1] + (self.capsule_tall, feature_view)
-        # batch | capsule tall | feature view
         return x.view(new_input_shape)
-
-    def interact_feature_view(self, x: Tensor):
-        first_copy_out = self.first_copy(x)
-        second_copy_out = self.second_copy(x)
-        third_copy_out = self.third_copy(x)
-
-        feature_interaction_score = torch.matmul(first_copy_out, second_copy_out.transpose(-2, -1))
-        feature_interaction_probability = nn.functional.softmax(feature_interaction_score, dim=-1)
-        feature_score_for_all_capsule = torch.matmul(feature_interaction_probability, third_copy_out)
-
-        # batch | capsule tall | feature view -> batch | capsule tall*feature view
-        return feature_score_for_all_capsule.view(x.shape[0], -1)
-    
+ 
     def forward(self, x: Tensor):
         previous_output = x
         for _ in range(self.capsule_wide):
-            # batch | input feature -> batch | capsule tall | feature view
-            previous_output_new_shape = self.apply_capsule_tall_dim(previous_output)
-            previous_output = self.interact_feature_view(previous_output_new_shape)
-        
-        output = self.output_decoder(previous_output)
-        return self.output_probability(output)
+            for layer in self.layers:
+                capsule_tall_dim_applied = self.apply_capsule_tall_dim(previous_output)
+                view_outputs = []
+                for input_view in range(capsule_tall_dim_applied.shape[1]):
+                    each_layer = layer[0]
+                    output = each_layer[input_view](capsule_tall_dim_applied[:, input_view, :])
+                    view_outputs.append(output)
+                previous_output = torch.concat(view_outputs, dim=1)
+
+        return self.output_decoder(previous_output)
