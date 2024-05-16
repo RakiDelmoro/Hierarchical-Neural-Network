1937fcb57b38ba22ba0433e5e3f197fe88ef041c
Raki Delmoro <rakidelmoro@gmail.com>
2024-05-16 09:01:38 +0000

    Update

--- capsule_nn_v2.py
+++ capsule_nn_v2.py
@@ -14,8 +14,10 @@ class CapsulePerceptron(nn.Module):
             output_feature = feature_sizes[i+1] // capsule_tall
             layer = nn.Sequential(nn.Linear(input_feature, output_feature, device="cuda"), nn.ReLU())
             self.layers.append(layer)
+        # This layer will be use when we concatenate the capsule column index
+        self.capsule_column_layer = nn.Linear(feature_sizes[0]+1, feature_sizes[0], device="cuda")
+        
         self.output_layer = nn.Linear(feature_sizes[-1], 10, device="cuda")
-
     def feature_view(self, x: Tensor):
         assert x.shape[-1] % self.capsule_tall == 0, f"Input feature {x.shape[-1]} should divisible by {self.capsule_tall}"
         feature_view = x.shape[-1] // self.capsule_tall
@@ -38,9 +40,15 @@ class CapsulePerceptron(nn.Module):
         
     def forward(self, x):
         previous_layer_output = x 
-        for _ in range(self.capsule_wide):
+        for capsule_column_index in range(self.capsule_wide):
             for layer_index, layer in enumerate(self.layers):
                 input_view = self.feature_view(previous_layer_output)
                 previous_layer_output = self.view_layer(input_view, layer, layer_index)
-
+            capsule_column_index = torch.full([x.shape[0], 1], capsule_column_index, device="cuda")
+            concatenated_column_index = torch.concat([previous_layer_output, capsule_column_index], dim=1)
+            previous_layer_output = self.capsule_column_layer(concatenated_column_index)
         return self.output_layer(previous_layer_output)
+
+# x = torch.randn(1, 784, device="cuda")
+# m = CapsulePerceptron(feature_sizes=[784, 2000, 784], capsule_wide=2, capsule_tall=4)
+# print(m(x))
