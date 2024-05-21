26d57a857947e74c97862939e31e3c3294219548
Raki Delmoro <rakidelmoro@gmail.com>
2024-05-21 01:08:11 +0000

    Update

--- functional_capsule_nn.py
+++ functional_capsule_nn.py
@@ -1,36 +1,41 @@
 import math
 import torch
-from torch import empty
 import torch.nn as nn
+from torch import empty
 from torch.nn import Parameter
 from torch.nn.functional import relu
 
 def capsule_neural_network(feature_sizes: list, input_feature: int, capsule_tall: int, capsule_wide: int):
+    input_features = feature_sizes[0]*capsule_tall + 1 + input_feature
+    assert input_features % capsule_tall == 0, f'{input_features} should divisible by {capsule_tall}'
+
     def linear_layer(in_features: int, out_features: int, device: str="cuda"):
         weight = Parameter(empty((out_features, in_features), device=device))
         bias = Parameter(empty(out_features, device=device))
+
         def linear_computation(x: torch.Tensor):
             return relu(torch.matmul(x, weight.t()) + bias)
-        
+
         def weight_and_bias_initialization():
             nn.init.kaiming_uniform_(weight, a=math.sqrt(5))
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(weight)
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
             nn.init.uniform_(bias, -bound, bound)
+        weight_and_bias_initialization()
 
-        weight_and_bias_initialization() 
         return linear_computation, weight, bias
 
     layers = []
     parameters: list[torch.nn.Parameter] = []
     for i in range(len(feature_sizes)-1):
-        input_feature = feature_sizes[i] + 1 if i != 0 else input_feature // capsule_tall + feature_sizes[0] + 1
-        output_feature = feature_sizes[i+1]
+        input_feature = feature_sizes[i] + 1 if i != 0 else (input_feature // capsule_tall) + feature_sizes[0] + 1
+        output_feature = feature_sizes[i+1] // capsule_tall
         layer, w, b = linear_layer(input_feature, output_feature)
         layers.append(layer)
         parameters.extend([w, b])
 
     def feature_view(x: torch.Tensor):
+        assert x.shape[-1] % capsule_tall == 0, f'{capsule_tall} should divisible by {x.shape[-1]}'
         input_view = x.shape[-1] // capsule_tall
         new_input_shape = x.shape[:-1] + (capsule_tall, input_view)
         return x.view(new_input_shape)
@@ -51,11 +56,11 @@ def capsule_neural_network(feature_sizes: list, input_feature: int, capsule_tall
         previous_layer_output = torch.zeros(size=(input_batch.shape[0], feature_sizes[0]*capsule_tall), device="cuda")
         for capsule_wide_idx in range(capsule_wide):
             for layer_idx, layer in enumerate(layers):
-                capsule_idx_as_tensor = torch.full([input_batch.shape[0], 1], capsule_wide_idx, device="cuda")
+                capsule_wide_idx_as_tensor = torch.full([input_batch.shape[0], 1], capsule_wide_idx, device="cuda")
                 if layer_idx == 0:
-                    input_for_layer = torch.concat([input_batch, previous_layer_output, capsule_idx_as_tensor], dim=1)
+                    input_for_layer = torch.concat([input_batch, previous_layer_output, capsule_wide_idx_as_tensor], dim=1)
                 else:
-                    input_for_layer = torch.concat([previous_layer_output, capsule_idx_as_tensor], dim=1)
+                    input_for_layer = torch.concat([previous_layer_output, capsule_wide_idx_as_tensor], dim=1)
                 input_feature_view = feature_view(input_for_layer)
                 previous_layer_output = viewed_layer(input_feature_view, layer, layer_idx)
         return previous_layer_output
